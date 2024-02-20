03ec510e2cbc7086a4b871d14c0f5e65360c9a94
Filip Pinjuh <filip.pinjuh@gmail.com>
2024-02-20 16:02:23 +0100

    player invite & game start

---
+++ README.md
@@ -0,0 +1,22 @@
+# D2API
+
+D2API is a simple API for creating custom matches in Dota2.
+
+## Running the API
+
+To run the API create an .env file in the root directory with the following content:
+
+```
+STEAM_USERNAME=your_steam_username
+STEAM_PASSWORD=your_steam_password
+```
+
+Then run the following commands:
+
+```Bash
+# Install the dependencies
+$ pip install -r requirements.txt
+
+# Run the script
+$ python main.py
+```
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from dota2.client import Dota2Client
 from dotenv import load_dotenv
 import os
 import time
+import logging
+
+# Set up logging
+logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
 
 # Load the environment variables
 load_dotenv()
@@ -19,8 +23,36 @@ def start_dota():
 
 @dota.on('ready')
 def create_practice_lobby():
-    get_lobby_list = dota.get_lobby_list()
-    print(get_lobby_list)
+    print('Destroying previous lobby...')
+    print(dota.destroy_lobby())
+
+    print('Sleeping for 5 seconds...')
+    time.sleep(5)
+
+    print('Creating lobby...')
+    dota.create_practice_lobby(password='1234', options={'game_name':'Relative', 'server_region':3})
+
+    # Make sure the lobby is created before we start adding players
+    print('Sleeping for 5 seconds...')
+    time.sleep(5)
+
+    print('Lobby created')
+    print(dota.lobby)
+
+    print('Adding players...')
+    dota.invite_to_lobby(steam_id=76561198176726223)
+
+
+@dota.on('lobby_changed')
+def lobby_changed(lobby):
+    print('Lobby changed')
+    print(lobby)
+
+    print('Sleeping for 5 seconds...')
+    time.sleep(5)
+
+    dota.launch_practice_lobby()
+    
 
 print('Logging in...')
 print("Username: ", STEAM_USERNAME)
